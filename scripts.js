a507804a45fabfaf4a5e571ca78c95abdadefce3
Miguel M Juan <mmjuan@iesperemaria.com>
2018-12-11 20:15:41 +0100

    primer commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function iniciar() {    
+    // medio = document.getElementById('medio');
+    
+    // medio.addEventListener('click', qwerty, false);
+}
+
+window.addEventListener('load', iniciar, false);
